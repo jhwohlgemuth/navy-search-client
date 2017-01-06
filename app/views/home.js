56762830e4e8db9336f1fd3111265193e746192d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-01-05 18:27:06 -0600

    Added search input flyout and started laying ground work for displaying results

--- app/views/home.js
+++ app/views/home.js
@@ -23,11 +23,24 @@ define(function(require, exports, module) {
         model: new Data.Model(),
         ui: {
             main: '#main',
+            searchInput: '.navy-search > input',
+            submitButton: '.submit-btn',
             aboutButton: '.about-btn'
         },
         events: {
             'click .about-btn': 'onClickAbout',
-            'touchstart .about-btn': 'onClickAbout'
+            'touchstart .about-btn': 'onClickAbout',
+            'click .submit-btn': 'onClickSubmit'
+        },
+        initialize: function() {
+            var view = this;
+            var RETURN_KEY_CODE = 13;
+            $(document).keypress(function(e) {
+                var key = e.which || e.keyCode;
+                if (key === RETURN_KEY_CODE && (view.ui.searchInput.val().length > 1)) {
+                    view.triggerMethod('click:submit');
+                }
+            });
         },
         onRender: function() {
 
@@ -35,6 +48,11 @@ define(function(require, exports, module) {
         onClickAbout: function() {
             this.ui.main.toggleClass('show-about');
             this.ui.aboutButton.toggleClass('active-btn');
+        },
+        onClickSubmit: function() {
+            this.ui.searchInput.toggleClass('fly-out--right');
+            this.ui.submitButton.toggleClass('processing');
+            this.ui.aboutButton.toggle();
         }
     });
 
